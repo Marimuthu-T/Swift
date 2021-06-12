1dd1dd146036f8ca8c5d9f1dea90f5445b32fb27
Marimuthu T <81249015+Marimuthu-T@users.noreply.github.com>
2021-06-12 09:29:52 +0530

    Update datatypes.swift

--- practices/datatypes.swift
+++ practices/datatypes.swift
@@ -1,1 +1,27 @@
 
+
+print("Welcome to Employee Details:")
+
+var EmployeeID=4756
+var EmployeeName="chrish"
+var Deparment="dev"
+var salary=67.500
+var availability=true
+
+print("EmployeeID: " + String(EmployeeID))
+
+
+var k=true
+print(k)
+k=false
+print(k)
+
+
+
+
+
+var implicitlyUnmrappedOptionsal: Int! = nil
+
+implicitlyUnmrappedOptionsal=23
+
+print(implicitlyUnmrappedOptionsal)
