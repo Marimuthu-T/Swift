28dbcdd4ffaeadf067b7e48feae26d92ef3b4115
Marimuthu T <81249015+Marimuthu-T@users.noreply.github.com>
2021-06-13 11:14:06 +0530

    Update datatypes.swift

--- practices/datatypes.swift
+++ practices/datatypes.swift
@@ -1,14 +1,33 @@
 
-
 print("Welcome to Employee Details:")
 
-var EmployeeID=4756
+var EmployeeID: UInt16 = 4756
 var EmployeeName="chrish"
 var Deparment="dev"
 var salary=67.500
 var availability=true
+var grade: Character = "s"
 
+print(grade)
 print("EmployeeID: " + String(EmployeeID))
+
+
+/*Curcumtance Calculator*/
+
+let pi=3.14
+var radius=6
+
+print("The circumference of radius \(radius) is : \(Double(2 * radius) * pi) ")
+
+
+
+
+var id: UInt8 = 255
+
+var longid: UInt16 = 3432
+
+print ( id &+ 23)
+
 
 
 var k=true
@@ -19,9 +38,11 @@ print(k)
 
 
 
-
 var implicitlyUnmrappedOptionsal: Int! = nil
 
 implicitlyUnmrappedOptionsal=23
 
 print(implicitlyUnmrappedOptionsal)
+
+
+
