1ba48ee96197bf29de1f7e3ea702166eaa6ff685
Marimuthu T <81249015+Marimuthu-T@users.noreply.github.com>
2021-06-13 19:30:53 +0530

    Update Arrays.swift

--- practices/Arrays.swift
+++ practices/Arrays.swift
@@ -1,1 +1,14 @@
 
+import Foundation
+
+ 
+let player = ["rock", "paper", "scissors", "lizard", "spock"]
+ print(player)
+ 
+ var deafultvalues = Array(repeating: 542, count: 34)
+ 
+ print(deafultvalues)
+ 
+ for (index, value) in player.enumerated() {
+    print("Item \(index + 1): \(value)")
+}
